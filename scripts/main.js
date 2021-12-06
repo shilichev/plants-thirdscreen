2aee25c3a2873a9f5bce4d1ffd99be02008b6434
ygug <yigig>
2021-12-06 15:19:56 +0300

    fixed 1

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,27 @@ function onClickEvent() {
     "https://apps.apple.com/by/app/plantme-plant-identification/id1532875668";
 }
 function addEvent() {
-  document.getElementById("button__first-screen").addEventListener("click", function () {
-    onClickEvent();
-  });
-  document.getElementById("button__second-screen").addEventListener("click", function () {
-    onClickEvent();
-  });
-  document.getElementById("button__third-screen").addEventListener("click", function () {
-    onClickEvent();
-  });
+  if (document.getElementById("button__first-screen")) {
+    document
+      .getElementById("button__first-screen")
+      .addEventListener("click", function () {
+        onClickEvent();
+      });
+  }
+  if (document.getElementById("button__second-screen")) {
+    document
+      .getElementById("button__second-screen")
+      .addEventListener("click", function () {
+        onClickEvent();
+      });
+  }
+  if (document.getElementById("button__third-screen")) {
+    document
+      .getElementById("button__third-screen")
+      .addEventListener("click", function () {
+        onClickEvent();
+      });
+  }
 }
 
 function documentReady() {
